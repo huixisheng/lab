82cae27895c089945f8d6705d23f439dadd82f8a
huixisheng <huixisheng@huixishengdeMacBook-Pro.local>
2018-01-26 16:38:29 +0800

    Squashed 'weex/webpack-demos/' content from commit 94a9dfc
    git-subtree-dir: weex/webpack-demos
    git-subtree-split: 94a9dfc4f818d97e7e38c4c5b37b84565bdfc0c8

---
+++ demo06/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './main.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules'
+      }
+    ]
+  }
+};
