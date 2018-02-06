edb8b0bc9d1877c235dd8206858d2f87ce382a74
huixisheng <huixisheng@huixishengdeMacBook-Pro.local>
2018-02-06 22:44:54 +0800

    feat: demo for qrcode-vue

---
+++ javascript/qrcode-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <qrcode-vue
+      :size="size"
+      :value="value"
+      :logo="logo"
+      :bgColor="bgColor"
+      :fgColor="fgColor"
+    ></qrcode-vue>
+  </div>
+</template>
+<script>
+  import qrcodeVue from 'qrcode-vue'
+  export default {
+    data () {
+      return {
+        size: 128,
+        bgColor: '#fff',
+        fgColor: '#000',
+        value: '15990112169',
+        logo: 'http://img0.cosmeapp.com/Fi_7Q66wn5LH8XWllY0YbIij86Rt'
+      }
+    },
+    components: {
+      qrcodeVue
+    }
+  }
+</script>
