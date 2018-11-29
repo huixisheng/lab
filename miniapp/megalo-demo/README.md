e3dc08c9a5e89ee79624f9ae7c3499c2f5a3c632
huixisheng <469184346@qq.com>
2018-11-29 14:38:49 +0800

    feat(megalo): add megalo demo

---
+++ miniapp/megalo-demo/README.md
@@ -0,0 +1,29 @@
+# megalo-demo
+
+## 描述
+
+megalo demo
+
+## 作者
+
+huixisheng <469184346@qq.com>
+
+## 运行
+
+微信小程序：
+
+```bash
+npm run dev:wechat
+```
+
+支付宝小程序：
+
+```bash
+npm run dev:alipay
+```
+
+百度智能小程序：
+
+```bash
+npm run dev:swan
+```
---
+++ miniapp/megalo-demo/build/createBaseConfig.js
@@ -0,0 +1,130 @@
+const createMegaloTarget = require( '@megalo/target' )
+const compiler = require( '@megalo/template-compiler' )
+const VueLoaderPlugin = require( 'vue-loader/lib/plugin' )
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' )
+const { pagesEntry, getSubPackagesRoot } = require('@megalo/entry')
+
+const _ = require( './util' );
+const appMainFile = _.resolve('src/app.js')
+const CSS_EXT = {
+  wechat: 'wxss',
+  alipay: 'acss',
+  swan: 'css',
+};
+
+function createBaseConfig( platform = 'wechat' ) {
+  const cssExt = CSS_EXT[platform]
+  
+  return {
+    mode: 'development',
+
+    target: createMegaloTarget( {
+      compiler: Object.assign( compiler, { } ),
+      platform,
+      htmlParse: {
+        templateName: 'octoParse',
+        src: _.resolve(`./node_modules/octoparse/lib/platform/${platform}`)
+      },
+      subPackages: getSubPackagesRoot(appMainFile)
+    } ),
+
+    entry: {
+      'app': appMainFile,
+      ...pagesEntry(appMainFile)
+    },
+
+    output: {
+      path: _.resolve( `dist-${platform}/` ),
+      filename: 'static/js/[name].js',
+      chunkFilename: 'static/js/[id].js'
+    },
+
+    devServer: {
+      // hot: true,
+    },
+
+    optimization: {
+      runtimeChunk: {
+        name: 'runtime'
+      },
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /\/node_modules\//,
+            name: 'vendor',
+            chunks: 'all'
+          }
+        }
+      }
+    },
+
+    // devtool: 'cheap-source-map',
+    devtool: false,
+
+    resolve: {
+      extensions: ['.vue', '.js', '.json'],
+      alias: {
+        'vue': 'megalo',
+        '@': _.resolve('src')
+      },
+    },
+
+    module: {
+      rules: [
+        // ... other rules
+        {
+          test: /\.vue$/,
+          use: [
+            {
+              loader: 'vue-loader',
+              options: {
+                a: 1,
+                cacheIdentifier: 'x'
+              }
+            }
+          ]
+        },
+
+        {
+          test: /\.js$/,
+          use: 'babel-loader'
+        },
+
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+
+        {
+          test: /\.less$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'less-loader',
+          ]
+        },
+
+      ]
+    },
+
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin( {
+        filename: `./static/css/[name].${cssExt}`,
+      } ),
+    ],
+    stats:{
+      env: true,
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      entrypoints: false,
+    }
+  }
+}
+
+module.exports = createBaseConfig
