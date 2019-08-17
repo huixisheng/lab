f7ba1796f6d75952798a422908fc74c3dcc96b1c
huixisheng <469184346@qq.com>
2019-08-18 01:25:10 +0800

    feat: taro-api-debug add example with get code、userInfo

--- miniapp/taro-api-debug/src/pages/index/index.tsx
+++ miniapp/taro-api-debug/src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
@@ -15,6 +15,12 @@ export default class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
+  state = {
+    wxLoginCode: '',
+    userInfo: '',
+    swanid: '',
+  }
+
   componentWillMount () { }
 
   componentDidMount () { }
@@ -25,10 +31,89 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  handleGetWxLoginCode = async () => {
+    const res = await Taro.login();
+    this.setState({
+      wxLoginCode: res.code,
+    });
+    const clipRes = await Taro.setClipboardData({
+      data: res.code,
+    });
+    if (clipRes) {
+      Taro.showToast({
+        title: '拷贝成功'
+      });
+    }
+  }
+
+  onGotUserInfo = async (e) => {
+    console.log(e);
+    const detail =  e.detail;
+    const userInfo = JSON.stringify(detail);
+    const clipRes = await Taro.setClipboardData({
+      data: userInfo,
+    });
+    this.setState({
+      userInfo,
+    });
+    if (clipRes) {
+      Taro.showToast({
+        title: '拷贝成功'
+      });
+    }
+  }
+
+  handleGetSwanId = async() => {
+    const swanid = await this.promiseGetSwanId();
+    const clipRes = await Taro.setClipboardData({
+      data: swanid,
+    });
+    this.setState({
+      swanid,
+    });
+    if (clipRes) {
+      Taro.showToast({
+        title: '拷贝成功'
+      });
+    }
+  }
+
+  promiseGetSwanId() {
+    return new Promise((resolve, reject) => {
+      swan.getSwanId({
+        success: function (res) {
+          resolve(res.data.swanid);
+          console.log('getSwanId success', res);
+        },
+        fail: function (err) {
+          reject(err);
+            console.log('getSwanId fail', err);
+        }
+      });
+    })
+
+  }
+
   render () {
+    const { wxLoginCode } = this.state;
+
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <View className="block">
+          <View>wx.login code:</View>
+          <View>{wxLoginCode}</View>
+          <Button type="primary" onClick={this.handleGetWxLoginCode.bind(this)}>复制微信|百度 code</Button>
+        </View>
+        <View className="block">
+          <View>用户信息:</View>
+          <View>{userInfo}</View>
+          <Button openType="getUserInfo" lang="zh_CN" onGetUserInfo={this.onGotUserInfo}>获取用户信息</Button>
+        </View>
+        <View className="block">
+          <View>百度swanid:</View>
+          <View>{swanid}</View>
+          <Button type="primary" onClick={this.handleGetSwanId}>获取百度 swanid</Button>
+        </View>
       </View>
     )
   }
