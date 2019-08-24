d4ac41f019140f23f2f45b41a23dc10adfd75e40
huixisheng <469184346@qq.com>
2019-08-24 14:15:06 +0800

    feat: flutter 编写第一个Flutter应用实例

--- flutter/flutter_demo/lib/main.dart
+++ flutter/flutter_demo/lib/main.dart
@@ -1,6 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp1());
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new RandomWords(),
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      // home: new Scaffold(
+      //   appBar: new AppBar(
+      //     title: new Text('Welcome to Flutter'),
+      //   ),
+      //   body: new Center(
+      //     // child: new Text('Hello World'),
+      //     // child: new Text(wordPair.asCamelCase)
+      //     child: new RandomWords()
+      //   ),
+      // ),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
+    // return new Text(wordPair.asPascalCase);
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+      // 在偶数行，该函数会为单词对添加一个ListTile row.
+      // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+      // 注意，在小屏幕上，分割线看起来可能比较吃力。
+      itemBuilder: (context, i) {
+        // 在每一列之前，添加一个1像素高的分隔线widget
+        if (i.isOdd) return new Divider();
+
+        // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+        // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+        final index = i ~/ 2;
+        // 如果是建议列表中最后一个单词对
+        if (index >= _suggestions.length) {
+          // ...接着再生成10个单词对，然后添加到建议列表
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -37,7 +169,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -47,18 +179,21 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    print('_counter $_counter');
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+      widget.title = '12';
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(context);
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
